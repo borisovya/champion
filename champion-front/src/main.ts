f676a5bda3c7d24e5cca669027fe2bc44242a4e2
Vladimir Borisov <v.borisov@uniteller.ru>
2024-01-30 19:58:59 +0300

    -

--- champion-front/src/main.ts
+++ champion-front/src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, { ripple: true });
+app.use(ToastService);
 
 app.mount('#app')
