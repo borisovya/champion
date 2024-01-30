2e852dd16871acc4363441f1d4b114384d047297
Vladimir Borisov <v.borisov@uniteller.ru>
2024-01-30 23:14:53 +0300

    added admin layout

--- champion-front/src/main.ts
+++ champion-front/src/main.ts
@@ -1,9 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
+import '@/assets/styles.scss';
 
 import App from './App.vue'
 import router from './router'
--- champion-front/src/router/index.ts
+++ champion-front/src/router/index.ts
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/pages/HomeView.vue'
+import AppLayout from '@/layout/AppLayout.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: AppLayout,
+      children:[
+        {
+          path: '/',
+          component: HomeView
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/pages/auth/Login.vue')
+    },
   ]
 })
 
