dbdbdd77a2519ec3280b76d216c767e7c6507ba8
icascada <v.abramov@uniteller.ru>
2024-02-10 23:45:40 +0400

    fix: swagger path

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -5,3 +5,7 @@ source './support/message.sh'
 message info 'The application restart process has started.'
 
 command docker compose up --force-recreate -d || { message error 'The application startup has failed' ; exit  1 ; }
+
+cd ../champion-backend || exit 1
+
+command php bin/console cache:clear
