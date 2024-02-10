91d371cb9d8af8140e3b6e4985226d8b4a4f7441
icascada <v.abramov@uniteller.ru>
2024-02-10 22:20:47 +0400

    feat: add error serializer

---
+++ champion-backend/src/Entity/User.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', unique: true)]
+    private string $email;
+
+    #[ORM\Column(type: 'string')]
+    private string $password;
+
+    #[ORM\Column(type: 'string')]
+    private string $telegramLogin;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $championPartnersLogin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): User
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelegramLogin(): string
+    {
+        return $this->telegramLogin;
+    }
+
+    public function setTelegramLogin(string $telegramLogin): User
+    {
+        $this->telegramLogin = $telegramLogin;
+
+        return $this;
+    }
+
+    public function getChampionPartnersLogin(): ?string
+    {
+        return $this->championPartnersLogin;
+    }
+
+    public function setChampionPartnersLogin(?string $championPartnersLogin): User
+    {
+        $this->championPartnersLogin = $championPartnersLogin;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): User
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return [];
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string)$this->id;
+    }
+}
